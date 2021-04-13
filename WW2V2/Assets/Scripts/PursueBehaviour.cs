944ca32db771fe0a19c4436538b7f7f7fd44eec1
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2021-04-13 21:28:21 +0100

    Created pursue behaviour

---
+++ WW2V2/Assets/Scripts/PursueBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PursueBehaviour : SteeringBehaviour
+{
+    public Ship enemyTarget;
+    public Vector3 enemyPos;
+
+    public void OnDrawGizmos()
+    {
+        if (Application.isPlaying && isActiveAndEnabled)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(transform.position, enemyPos);
+        }
+    }
+
+    public override Vector3 Calculate()
+    {
+        float dist = Vector3.Distance(enemyTarget.transform.position, transform.position);
+        float time = dist / ship.maxSpeed;
+
+        enemyPos = enemyTarget.transform.position + (enemyTarget.velocity * time);
+
+        return ship.SeekForce(enemyPos);
+    }
+}
