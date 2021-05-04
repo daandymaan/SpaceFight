6de4733874d0931570ef880084e5f69cfe8e3a11
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2021-05-04 14:16:37 +0100

    Messerschmitt class inherits for ShipSystems

---
+++ WW2V2/Assets/Scripts/Messerschmitt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Messerschmitt : ShipSystems
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<StateMachine>().ChangeState(new Cruise());
+        GetComponent<StateMachine>().SetGlobalState(new Alive());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
