7aa65d002dc8a6d7e283c2c1ff70fa347e96f231
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2021-05-04 22:08:26 +0100

    Implemente motion in the propeller of the spitfire

---
+++ WW2V2/Assets/Scripts/PropellerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerBehaviour : MonoBehaviour
+{
+    public float propellerSpeed = 750f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.localRotation *= Quaternion.AngleAxis(propellerSpeed * Time.deltaTime, Vector3.back);
+    }
+}
