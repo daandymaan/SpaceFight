53e413adf295bd274474f3374fdc553ae6369f5f
Dan Simons <47121977+daandymaan@users.noreply.github.com>
2021-05-09 12:50:03 +0100

    Added engine functionality to detect shots fired Also made the laser speed public for altering Implemented cannon fire functionality

---
+++ WW2V2/Assets/Scripts/EngineBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.gameObject.tag == "o_laser")
+        {
+            GameObject.Find("McCannon").GetComponent<ShipSystems>().health = GameObject.Find("McCannon").GetComponent<ShipSystems>().health - 5; 
+        } 
+    }
+}
--- WW2V2/Assets/Scripts/McCannon.cs
+++ WW2V2/Assets/Scripts/McCannon.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class McCannon : ShipSystems
 {
     public GameObject messerschmittShip;
-    public int maxShips = 0;
+    private int shipCount = 0;
+    public float spawnDelay = 20f;
     Transform spawnpoint1;
     Transform spawnpoint2;
+    private int maxShips = 6;
+    public AudioSource siren;
+    public AudioSource charge;
     void Awake()
     {
         
@@ -19,6 +23,7 @@ public class McCannon : ShipSystems
         StartCoroutine(leaderDetectionCouroutine());
         StartCoroutine(enemyDetectionCouroutine());
         StartCoroutine(deployDefence());
+        StartCoroutine(fireCannons());
     }
     void Start()
     {
@@ -28,7 +33,10 @@ public class McCannon : ShipSystems
     // Update is called once per frame
     void Update()
     {
-
+        if(health < 10)
+        {
+            explodeShip();
+        }
     }
     public new GameObject getLeader()
     {
@@ -41,17 +49,71 @@ public class McCannon : ShipSystems
     {
         while(true)
         {
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(spawnDelay);
             if(targetEnemy != null)
             {
                 float dist = Vector3.Distance(targetEnemy.transform.position, transform.position);
-                if(dist < detectionRange && maxShips < 4)
+                if(dist < detectionRange && shipCount < maxShips)
                 {
+                    if(!siren.isPlaying)
+                    {
+                        siren.Play();
+                    }
                     Instantiate(messerschmittShip, spawnpoint1.position, transform.rotation);
                     Instantiate(messerschmittShip, spawnpoint2.position, transform.rotation);
+                    spawnDelay += spawnDelay;
                 }
             }
-            maxShips = GameObject.FindGameObjectsWithTag("jibinis").Length;
+            shipCount = GameObject.FindGameObjectsWithTag("jibinis").Length;
         }
+    }
+
+    public new IEnumerator leaderDetectionCouroutine()
+    {
+        while(true)
+        {
+            if(targetLeader == null)
+            {
+                targetLeader = getLeader();
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator fireCannons()
+    {
+        while(true) 
+        {
+            charge.Play();
+            yield return new WaitForSeconds(4f);
+            shootFX.Play();
+            yield return new WaitForSeconds(0.5f);
+            shoot();
+            yield return new WaitForSeconds(6f);
+        }
+    }
+
+    void explodeShip()
+    {
+        GameObject explosionObj = transform.Find("ExplosionMC").gameObject;
+        int explosionCount = explosionObj.transform.childCount;
+        for(int i = 0; i < explosionCount; i++)
+        {
+            if(!explosion.isPlaying)
+            {
+                explosion.Play();
+            }
+            explosionObj.transform.GetChild(i).gameObject.transform.Find("BigExplosion").GetComponent<ParticleSystem>().Play();
+        }
+        float totalTime = 4f;
+        Destroy(gameObject, totalTime);
+    }
+
+    void shoot()
+    {
+        Instantiate(bulletPrefab, transform.Find("Cannons").gameObject.transform.GetChild(0).gameObject.transform.position, transform.rotation);
+        Instantiate(bulletPrefab, transform.Find("Cannons").gameObject.transform.GetChild(1).gameObject.transform.position, transform.rotation);
+        Instantiate(bulletPrefab, transform.Find("Cannons").gameObject.transform.GetChild(2).gameObject.transform.position, transform.rotation);
+        Instantiate(bulletPrefab, transform.Find("Cannons").gameObject.transform.GetChild(3).gameObject.transform.position, transform.rotation);
     }
 }
